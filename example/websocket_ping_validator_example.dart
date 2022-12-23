449870d27b292fb579405b95acef1846d900e208
flerma <fiernandoalberto@gmail.com>
2022-12-23 12:27:16 -0700

    first commit

---
+++ example/websocket_ping_validator_example.dart
@@ -0,0 +1,49 @@
+import 'dart:typed_data';
+
+import 'package:websocket_ping_validator/websocket_ping_validator.dart';
+
+void main() async {
+  ///Type: String
+  ///YOUR WEBSOCKET URL
+  const url = "ws...";
+
+  ///Type: bool
+  ///VARIABLE SPECIFICITIES IF YOU WANT TO RECONNECT WHEN A ERROR HAVE OCCURRED
+  const reconnectOnError = true;
+
+  ///Type: dynamic (NOT NULL)
+  ///IS THE DATA SENT TO SERVER TO MAKE A PING
+  final dataToSendAsPing = Uint8List.fromList([0]);
+
+  ///Type: int
+  ///IS THE MAX COUNT OF ATTEMPTS TO MAKE A PING
+  const maxAttempts = 5;
+
+  ///Type: Duration
+  ///IS DURATION OF TIME TO MAKE A PING
+  const periodicDurationToPing = Duration(seconds: 3);
+
+  ///Type: Duration
+  ///IS DURATION OF TIME TO RECONNECT THE WEBSOCKET
+  const reconnectIn = Duration(seconds: 3);
+
+  await WebsocketPingValidator.connectWebSocket(url,
+      onMessage: (message) async {
+    ///MESSAGE RECEIVED FROM SERVER
+  }, onConnected: (dateTimeConnected) async {
+    ///WEBSOCKET CONNECTED (YOU RECEIVE THE SPECIFIC DateTime WHEN THE CONNECTION OCCURRED)
+  }, onConnectionClosed: (statusCode) async {
+    ///WEBSOCKET DISCONNECTED (YOU RECEIVE THE SPECIFIC StatusCode IN int WHEN THE DISCONNECTION OCCURRED)
+  }, onConnectionLost: () async {
+    ///WEBSOCKET HAVE LOST THEIR CONNECTION
+  }, onError: (error) async {
+    ///WEBSOCKET HAVE OCCURRED AN ERROR (YOU RECEIVED IT)
+  }, onReconnectStarted: (duration) async {
+    ///FUNCTION BEFORE START RECONNECTION COUNT DOWN
+  },
+      maxAttempts: maxAttempts,
+      periodicDurationToPing: periodicDurationToPing,
+      reconnectIn: reconnectIn,
+      reconnectOnError: reconnectOnError,
+      dataToSendAsPing: dataToSendAsPing);
+}
---
+++ lib/websocket_ping_validator.dart
@@ -0,0 +1,131 @@
+library websocket_ping_validator;
+
+import 'dart:async';
+import 'dart:io';
+
+abstract class WebsocketPingValidator {
+  static Future<void> connectWebSocket(final String url,
+      {required final Function(dynamic) onMessage,
+      final Function(DateTime)? onConnected,
+      final Function(Object)? onError,
+      final Function(int)? onConnectionClosed,
+      final Function? onConnectionLost,
+      final Function(Duration)? onReconnectStarted,
+      required final bool reconnectOnError,
+      required final dynamic dataToSendAsPing,
+      final int maxAttempts = 5,
+      final Duration periodicDurationToPing = const Duration(seconds: 3),
+      final Duration reconnectIn = const Duration(seconds: 3)}) async {
+    try {
+      int retry = 1;
+      final webSocketConnection = await WebSocket.connect(url);
+      if (onConnected != null) await onConnected(DateTime.now());
+      webSocketConnection.listen((message) async {
+        retry = 1;
+        await onMessage(message);
+      }, onError: (error) async {
+        if (onError != null) {
+          await onError(error);
+        }
+        if (reconnectOnError) {
+          await _reconnect(url,
+              onMessage: onMessage,
+              onConnected: onConnected,
+              onError: onError,
+              onConnectionClosed: onConnectionClosed,
+              onConnectionLost: onConnectionLost,
+              onReconnectStarted: onReconnectStarted,
+              reconnectOnError: reconnectOnError,
+              dataToSendAsPing: dataToSendAsPing,
+              maxAttempts: maxAttempts,
+              periodicDurationToPing: periodicDurationToPing,
+              reconnectIn: reconnectIn);
+        }
+      }, onDone: () async {
+        if (onConnectionClosed != null) {
+          await onConnectionClosed(
+              webSocketConnection.closeCode ?? WebSocketStatus.normalClosure);
+        }
+      }, cancelOnError: true);
+
+      if (dataToSendAsPing != null) {
+        Timer.periodic(periodicDurationToPing, (timer) async {
+          try {
+            webSocketConnection.add(dataToSendAsPing);
+            if (retry >= maxAttempts) {
+              timer.cancel();
+              if (onConnectionLost != null) {
+                await onConnectionLost();
+              }
+              await webSocketConnection.close();
+              await _reconnect(url,
+                  onMessage: onMessage,
+                  onConnected: onConnected,
+                  onError: onError,
+                  onConnectionClosed: onConnectionClosed,
+                  onConnectionLost: onConnectionLost,
+                  onReconnectStarted: onReconnectStarted,
+                  reconnectOnError: reconnectOnError,
+                  dataToSendAsPing: dataToSendAsPing,
+                  maxAttempts: maxAttempts,
+                  periodicDurationToPing: periodicDurationToPing,
+                  reconnectIn: reconnectIn);
+              return;
+            }
+            retry += 1;
+          } catch (error) {
+            ///IGNORE
+          }
+        });
+      }
+    } catch (error) {
+      if (onError != null) {
+        await onError(error);
+      }
+      if (reconnectOnError) {
+        await _reconnect(url,
+            onMessage: onMessage,
+            onConnected: onConnected,
+            onError: onError,
+            onConnectionClosed: onConnectionClosed,
+            onConnectionLost: onConnectionLost,
+            onReconnectStarted: onReconnectStarted,
+            reconnectOnError: reconnectOnError,
+            dataToSendAsPing: dataToSendAsPing,
+            maxAttempts: maxAttempts,
+            periodicDurationToPing: periodicDurationToPing,
+            reconnectIn: reconnectIn);
+      }
+    }
+  }
+
+  static Future<void> _reconnect(final String url,
+      {required final Function(dynamic) onMessage,
+      final Function(DateTime)? onConnected,
+      final Function(Object)? onError,
+      final Function(int)? onConnectionClosed,
+      final Function? onConnectionLost,
+      final Function(Duration)? onReconnectStarted,
+      required final bool reconnectOnError,
+      required final dynamic dataToSendAsPing,
+      final int maxAttempts = 5,
+      final Duration periodicDurationToPing = const Duration(seconds: 3),
+      final Duration reconnectIn = const Duration(seconds: 3)}) async {
+    if (onReconnectStarted != null) {
+      await onReconnectStarted(reconnectIn);
+    }
+    await Future.delayed(reconnectIn);
+    await connectWebSocket(url,
+        onMessage: onMessage,
+        onConnected: onConnected,
+        onError: onError,
+        onConnectionClosed: onConnectionClosed,
+        onConnectionLost: onConnectionLost,
+        onReconnectStarted: onReconnectStarted,
+        reconnectOnError: reconnectOnError,
+        dataToSendAsPing: dataToSendAsPing,
+        maxAttempts: maxAttempts,
+        periodicDurationToPing: periodicDurationToPing,
+        reconnectIn: reconnectIn);
+  }
+}
