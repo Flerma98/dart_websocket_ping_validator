1bbd71ce4b295c3df75ae7dcfaedc285013e77a0
flerma <fiernandoalberto@gmail.com>
2022-12-26 17:06:21 -0700

    v1.0.6 done

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.6
+- On new instance fixed stopping previews instance.
+
 ## 1.0.5
 - On new instance callback created.
 
--- lib/websocket_ping_validator.dart
+++ lib/websocket_ping_validator.dart
@@ -40,26 +40,53 @@ abstract class WebsocketPingValidator {
       if (properties.dataToSendAsPing != null) {
         Timer.periodic(properties.periodicDurationToPing, (timer) async {
           try {
+            if (webSocketConnection.closeCode != null) {
+              timer.cancel();
+              return;
+            }
+
             if (properties.onAttemptChanged != null) {
               await properties.onAttemptChanged!(attempt);
             }
 
-            webSocketConnection.add(properties.dataToSendAsPing);
+            if (webSocketConnection.closeCode == null) {
+              webSocketConnection.add(properties.dataToSendAsPing);
+            }
 
             if (attempt >= properties.maxAttempts) {
               timer.cancel();
               if (properties.onConnectionLost != null) {
                 await properties.onConnectionLost!();
               }
-              await webSocketConnection.close();
+
+              if (webSocketConnection.closeCode == null) {
+                await webSocketConnection.close();
+              }
+
               await properties.onNewInstanceCreated(
                   await _reconnect(url, properties: properties));
               return;
             }
           } catch (error) {
+            timer.cancel();
+
             if (properties.onError != null) {
               await properties.onError!(error);
             }
+
+            if (properties.onConnectionLost != null) {
+              await properties.onConnectionLost!();
+            }
+
+            if (webSocketConnection.closeCode == null) {
+              await webSocketConnection.close();
+            }
+
+            if (properties.reconnectOnError) {
+              await properties.onNewInstanceCreated(
+                  await _reconnect(url, properties: properties));
+            }
+            return;
           }
           attempt = attempt += 1;
         });
